8f806f1ed7ebea761b2b16143538854ee74378ba
DESKTOP-SRH54VT\emilio <emonteror@ucenfotec.ac.cr>
2019-03-18 10:28:32 -0600

    new classes
    added classes:
    - Vec
    - Rect
    - Ball extends Rect

--- pong.js
+++ pong.js
@@ -1,7 +1,34 @@
+class Vec {
+    constructor(x = 0, y= 0) {
+        this.x = x
+        this.y = y
+    }
+}
+
+class Rect {
+    constructor(width, height) {
+        this.pos = new Vec;
+        this.size = new Vec(width, height)
+    }
+}
+
+class Ball extends Rect {
+    constructor() {
+        super(10,10)
+        this.vel = new Vec;
+    }
+}
+
+const black = '#000'
+const white = '#fff'
+
 const canvas = document.getElementById('pong')
 const context = canvas.getContext('2d')
- 
-const black = '#000'
-context.fillStyle = black;
+const ball = new Ball;
 
+console.log(ball)
+
+context.fillStyle = black
 context.fillRect(0, 0, canvas.clientWidth, canvas.height)
+context.fillStyle = white
+context.fillRect(0, 0, 10, 10)
