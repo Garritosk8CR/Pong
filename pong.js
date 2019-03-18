2d0ab2b09b222ad5de4d7f6c1e92def18c72b94e
DESKTOP-SRH54VT\emilio <emonteror@ucenfotec.ac.cr>
2019-03-17 18:10:50 -0600

    canvas

--- pong.js
+++ pong.js
@@ -1,1 +1,7 @@
 const canvas = document.getElementById('pong')
+const context = canvas.getContext('2d')
+ 
+const black = '#000'
+context.fillStyle = black;
+
+context.fillRect(0, 0, canvas.clientWidth, canvas.height)
