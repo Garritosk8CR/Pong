636d81c17760f1abbbc6e30403af1212ee5eb47a
DESKTOP-SRH54VT\emilio <emonteror@ucenfotec.ac.cr>
2019-03-18 12:52:02 -0600

    Pong class

--- pong.js
+++ pong.js
@@ -17,37 +17,68 @@ class Ball extends Rect {
         super(10,10)
         this.vel = new Vec;
     }
+
+    get left() {
+        return this.pos.x - this.size.x / 2
+    }
+
+    get right() {
+        return this.pos.x + this.size.x / 2
+    }
+
+    get top() {
+        return this.pos.y - this.size.y / 2
+    }
+
+    get bottom() {
+        return this.pos.y + this.size.y / 2
+    }
 }
 
-const black = '#000'
-const white = '#fff'
+class Pong {
+
+    constructor(canvas) {
+        this._canvas = canvas
+        this._context = canvas.getContext('2d')
+        this.ball = new Ball;
+        this.ball.pos.x = 100
+        this.ball.pos.y = 50
+        this.ball.vel.x = 100
+        this.ball.vel.y = 100
+        this.black = '#000'
+        this.white = '#fff'
+        let lastTime;
+        const callback = (milliSeconds) => {
+            if(lastTime) {
+                this.update((milliSeconds - lastTime) / 1000)
+            }
+            lastTime = milliSeconds
+            requestAnimationFrame(callback)
+        }
+        callback()
+    }
+
+    update(deltaTime) {
+        this.ball.pos.x += this.ball.vel.x * deltaTime
+        this.ball.pos.y += this.ball.vel.y * deltaTime
+    
+        if(this.ball.left < 0 || this.ball.right > this._canvas.width) {
+            //invert
+            this.ball.vel.x = -this.ball.vel.x
+        }
+        if(this.ball.top < 0 || this.ball.bottom > this._canvas.height) {
+            //invert
+            this.ball.vel.y = -this.ball.vel.y
+        }
+        this._context.fillStyle = this.black
+        this._context.fillRect(0, 0, this._canvas.width, this._canvas.height)
+        this._context.fillStyle = this.white
+        this._context.fillRect(this.ball.pos.x, this.ball.pos.y, this.ball.size.x, this.ball.size.y)
+        console.log(this.ball)
+    }
+}
+
+
 
 const canvas = document.getElementById('pong')
-const context = canvas.getContext('2d')
-const ball = new Ball;
-ball.pos.x = 100
-ball.pos.y = 50
-ball.vel.x = 100
-ball.vel.y = 100
-
-let lastTime;
-function callback(milliSeconds) {
-    if(lastTime) {
-        update((milliSeconds - lastTime) / 1000)
-    }
-    lastTime = milliSeconds
-    requestAnimationFrame(callback)
-}
-
-function update(deltaTime) {
-    ball.pos.x += ball.vel.x * deltaTime
-    ball.pos.y += ball.vel.y * deltaTime
-    context.fillStyle = black
-    context.fillRect(0, 0, canvas.width, canvas.height)
-    context.fillStyle = white
-    context.fillRect(ball.pos.x, ball.pos.y, ball.size.x, ball.size.y)
-    console.log(ball)
-}
-
-
-callback()
+const pong = new Pong(canvas)
